82663c6f2d3455a7ae7cf784f4ec5c0a741b085c
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-04-12 22:21:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PLASMA USA
+
+- Plasma donor screening page
+- Alerts screener when input is out of range or invalid
+- Pass the donor if vitals are all valid after pressing the check result button
---
+++ script.js
@@ -0,0 +1,19 @@
+const weight = document.getElementById("weight");
+const armCheck = document.getElementById("arm-check");
+const temperature = document.getElementById("temperature");
+const pulse = document.getElementById("pulse");
+const sys = document.getElementById("sys");
+const dia = document.getElementById("dia");
+const hematocrit = document.getElementById("hematocrit");
+const totalProtein = document.getElementById("total-protein");
+
+function checkWeight() {
+  if (weight.value > 110 && weight.value < 400) {
+    document.getElementById("results-status").innerHTML = "PASS";
+    weight.innerHTML = weight.value;
+  } else {
+    document.getElementById("results-status").innerHTML = "FAIL";
+  }
+}
+
+weight.addEventListener("input", checkWeight);
