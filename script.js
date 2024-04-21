03c24d801134c04123ee92cdc1e5c35f7672870d
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-04-20 22:02:38 -0400

    updated validation

--- script.js
+++ script.js
@@ -10,14 +10,18 @@ const resultBtn = document.getElementById("result-btn");
 
 let passedVitals = [];
 
-// USE TRY CATCH INSTEAD OF IF STATEMENTS
 function checkWeight() {
   if (weight.value > 109 && weight.value < 401) {
     document.getElementById("weight-result").innerHTML = "PASS";
     document.getElementById("weight").innerHTML = `${weight.value}  lbs`;
     passedVitals.push("weight");
-  } else {
+  } else if (weight.value < 109 || weight.value > 401) {
     document.getElementById("weight-result").innerHTML = "FAIL";
+    document.getElementById("weight").innerHTML = `${weight.value}  lbs`;
+  } else {
+    document
+      .getElementById("weight")
+      .appendChild(document.createElement("p")).innerHTML = "Must be a number.";
   }
 }
 
@@ -28,11 +32,14 @@ function checkArmCheck() {
     passedVitals.push("armCheck");
   } else if (armCheck.value === "F" || armCheck.value === "f") {
     document.getElementById("arm-result").innerHTML = "FAIL";
+    document.getElementById("arm-check").innerHTML = `${armCheck.value}`;
+  } else {
+    document
+      .getElementById("arm-check")
+      .appendChild(
+        document.createElement("p")
+      ).innerHTML = `Must be "P" or "F".`;
   }
-  // else {
-  //   document.getElementById("arm-check").appendChild("INVALID").innerHTML =
-  //     "INVALID";
-  // }
 }
 
 function checkTemperature() {
@@ -43,8 +50,16 @@ function checkTemperature() {
       "temperature"
     ).innerHTML = `${temperature.value}  &#x2109;`;
     passedVitals.push("temperature");
-  } else {
+  } else if (temperature.value < 94.9 || temperature.value > 99.6) {
     document.getElementById("temp-result").innerHTML = "FAIL";
+    document.getElementById(
+      "temperature"
+    ).innerHTML = `${temperature.value}  &#x2109;`;
+  } else {
+    document
+      .getElementById("temperature")
+      .appendChild(document.createElement("p")).innerHTML =
+      "Must be a decimal.";
   }
 }
 
@@ -53,8 +68,13 @@ function checkPulse() {
     document.getElementById("pulse-result").innerHTML = "PASS";
     document.getElementById("pulse").innerHTML = `${pulse.value}  bpm`;
     passedVitals.push("pulse");
-  } else {
+  } else if (pulse.value < 49 || pulse.value > 101) {
     document.getElementById("pulse-result").innerHTML = "FAIL";
+    document.getElementById("pulse").innerHTML = `${pulse.value}  bpm`;
+  } else {
+    document
+      .getElementById("pulse")
+      .appendChild(document.createElement("p")).innerHTML = "Must be a number.";
   }
 }
 
@@ -63,8 +83,13 @@ function checkSys() {
     document.getElementById("sys-result").innerHTML = "PASS";
     document.getElementById("sys").innerHTML = `${sys.value}  mmHg`;
     passedVitals.push("sys");
-  } else {
+  } else if (sys.value < 89 || sys.value > 181) {
     document.getElementById("sys-result").innerHTML = "FAIL";
+    document.getElementById("sys").innerHTML = `${sys.value}  mmHg`;
+  } else {
+    document
+      .getElementById("sys")
+      .appendChild(document.createElement("p")).innerHTML = "Must be a number.";
   }
 }
 
@@ -73,8 +98,13 @@ function checkDia() {
     document.getElementById("dia-result").innerHTML = "PASS";
     document.getElementById("dia").innerHTML = `${dia.value}  mmHg`;
     passedVitals.push("dia");
-  } else {
+  } else if (dia.value < 49 || dia.value > 101) {
     document.getElementById("dia-result").innerHTML = "FAIL";
+    document.getElementById("dia").innerHTML = `${dia.value}  mmHg`;
+  } else {
+    document
+      .getElementById("dia")
+      .appendChild(document.createElement("p")).innerHTML = "Must be a number.";
   }
 }
 
@@ -85,8 +115,15 @@ function checkHematocrit() {
       "hematocrit"
     ).innerHTML = `${hematocrit.value}  &#37;`;
     passedVitals.push("hematocrit");
-  } else {
+  } else if (hematocrit.value < 38 || hematocrit.value > 55) {
     document.getElementById("hematocrit-result").innerHTML = "FAIL";
+    document.getElementById(
+      "hematocrit"
+    ).innerHTML = `${hematocrit.value}  &#37;`;
+  } else {
+    document
+      .getElementById("hematocrit")
+      .appendChild(document.createElement("p")).innerHTML = "Must be a number.";
   }
 }
 
@@ -97,8 +134,15 @@ function checkTotalProtein() {
       "total-protein"
     ).innerHTML = `${totalProtein.value}  g/dl`;
     passedVitals.push("totalProtein");
-  } else {
+  } else if (totalProtein.value < 3.9 || totalProtein.value > 9.1) {
     document.getElementById("protein-result").innerHTML = "FAIL";
+    document.getElementById(
+      "total-protein"
+    ).innerHTML = `${totalProtein.value}  g/dl`;
+  } else {
+    document
+      .getElementById("total-protein")
+      .appendChild(document.createElement("p")).innerHTML = "Must be a number.";
   }
 }
 
